8cbec6362367bd560caa072d16d4799a1bce4403
Jie <1007172767@qq.com>
2017-09-16 23:40:32 +0800

    zuoye

--- first/flower.py
+++ first/flower.py
@@ -1,18 +1,55 @@
-from turtle import *
-def move():
-    for i in range(144):
-        right(1)
-        forward(1)
-color('red','pink')
-begin_fill()
-for _ in range(5):
-    left(72)
-    move()
-end_fill()
-color('yellow','yellow')
-begin_fill()
-up()
-forward(30)
+import turtle
+t = turtle.Turtle()
 
-end_fill()
-done()
+
+def move(a, b, c):
+    for i in range(a):
+        t.right(b)
+        t.forward(c)
+
+
+def set_pos(x, y):
+    t.up()
+    t.goto(x, y)
+    t.down()
+
+
+def leaf(deg):
+    t.pensize(1)
+    t.color("green")
+    t.begin_fill()
+    t.left(deg)
+    move(75, 1, 2)
+    t.right(105)
+    move(75, 1, 2)
+    t.end_fill()
+
+
+def gen(height):
+    t.color("brown")
+    t.pensize(10)
+    t.forward(height)
+
+
+t.color("red")
+t.pensize(5)
+set_pos(-100, 100)
+t.speed(100)
+for _ in range(5):
+    t.left(72)
+    move(144, 2, 2)
+set_pos(-80, 90)
+t.right(180)
+t.circle(20, 360)
+set_pos(-80, -15)
+t.left(90)
+gen(75)
+t.left(90)
+leaf(75)
+t.left(90)
+gen(50)
+t.right(90)
+leaf(0)
+t.right(15)
+gen(75)
+turtle.done()
--- first/try.py
+++ first/try.py
@@ -1,1 +1,18 @@
-print("hello world")
+import turtle
+
+def move(a, b, c):
+    for i in range(a):
+        turtle.right(b)
+        turtle.forward(c)
+def leaf(deg):
+    turtle.pensize(1)
+    turtle.color("green")
+    turtle.begin_fill()
+    turtle.left(deg)
+    move(75,1,2)
+    turtle.right(105)
+    move(75,1,2)
+    turtle.end_fill()
+leaf(75)
+leaf(0)
+turtle.done()
